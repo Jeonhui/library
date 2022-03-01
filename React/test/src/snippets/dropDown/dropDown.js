301a221d9b028a743a2f58ac61d1934c5c0be9be
Jeonhui <dlwjsgml02@naver.com>
2022-03-01 16:18:32 +0900

    add DropDown

--- React/test/src/snippets/dropDown/dropDown.js
+++ React/test/src/snippets/dropDown/dropDown.js
@@ -1,14 +1,15 @@
 import "./dropDown.css"
 
 export function DropDown() {
+
     const MouseOver = () => {
         const menu = window.document.getElementById("dropDownMenu");
         menu.style.display = "block"
-    }
+    } //mouseover시 나타내기
     const MouseOut = () => {
         const menu = window.document.getElementById("dropDownMenu");
         menu.style.display = "none"
-    }
+    } //mouseout시 사라지기
 
     return (
         <div className="dropDown" onMouseOver={MouseOver} onMouseOut={MouseOut}>
@@ -20,7 +21,6 @@ export function DropDown() {
                 <li>b</li>
                 <li>c</li>
             </ol>
-
         </div>
     );
 }
--- React/test/src/snippets/index.js
+++ React/test/src/snippets/index.js
@@ -1,5 +1,6 @@
 /* 밑에 추가해서 사용 */
-/* {} */
+/* App.js -> import {파일명} from 'snippets' 로 사용 */
 /* snippets 내보내기 */
+
 export * from './dropDown/dropDown'
 
